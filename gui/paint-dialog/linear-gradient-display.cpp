36e09dc937f8966e3d6f34423ae8ca9fa34d2264
Lukas Kemmer <lukas.kemmer@gmail.com>
2015-04-08 19:38:47 +0200

    Removed unnecessary include.

--- gui/paint-dialog/linear-gradient-display.cpp
+++ gui/paint-dialog/linear-gradient-display.cpp
@@ -13,7 +13,6 @@
 // implied. See the License for the specific language governing
 // permissions and limitations under the License.
 
-#include <memory>
 #include "wx/bitmap.h"
 #include "wx/dcclient.h"
 #include "wx/window.h" // For allowing Bind
