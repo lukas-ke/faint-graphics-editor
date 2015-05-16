92d523bb9651abaca785a5f478e00a4a0d6d2b24
Lukas Kemmer <lukas.kemmer@gmail.com>
2015-05-16 13:38:21 +0200

    Improved documentation formatting.

--- help/example_py/python_bind_keys_lambda.py
+++ help/example_py/python_bind_keys_lambda.py
@@ -1,4 +1,5 @@
 from ifaint import *
 
 #start
-bindc('r',lambda: set_fg(255,0,0),mod.shift) # Set red foreground color with Shift+R
+# Set red foreground color with Shift+R
+bindc('r',lambda: set_fg(255,0,0),mod.shift)
