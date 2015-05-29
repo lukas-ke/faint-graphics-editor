61ae778349f09609b7eeab77948a1c0a36ddb83e
Lukas Kemmer <lukas.kemmer@gmail.com>
2015-05-29 10:31:45 +0200

    Simplified add-gui-tests.cpp with lambda.

--- tests/gui-tests/add-gui-tests.cpp
+++ tests/gui-tests/add-gui-tests.cpp
@@ -22,58 +22,27 @@ using test_init_func_t = void(*)(wxWindow*,
   faint::StatusInterface&,
   faint::DialogContext&);
 
-// Fixme: Replace with lambda
-static void add_test(const faint::utf8_string& name,
-  test_init_func_t f,
-  wxBookCtrlBase* pageList,
-  faint::StatusInterface& statusInterface,
-  faint::DialogContext& dialogContext)
-{
-  auto* p = faint::raw(faint::create_panel(pageList));
-  f(p, statusInterface, dialogContext);
-  add_page(pageList, p, name);
-}
-
 void add_gui_tests(wxBookCtrlBase* pageList,
   faint::StatusInterface& statusInterface,
   faint::DialogContext& dialogContext)
 {
-  add_test("AlphaDialog", gui_test_alpha_dialog,
-    pageList, statusInterface, dialogContext);
+  auto add_test = [&](const faint::utf8_string& name, test_init_func_t f){
+    auto* p = faint::raw(faint::create_panel(pageList));
+    f(p, statusInterface, dialogContext);
+    add_page(pageList, p, name);
+  };
 
-  add_test("BitmapListCtrl", gui_test_bitmap_list_ctrl,
-    pageList, statusInterface, dialogContext);
-
-  add_test("GradientPanel", gui_test_gradient_panel,
-    pageList, statusInterface, dialogContext);
-
-  add_test("GridDialog", gui_test_grid_dialog,
-    pageList, statusInterface, dialogContext);
-
-  add_test("HSLPanel", gui_test_hsl_panel,
-    pageList, statusInterface, dialogContext);
-
-  add_test("ImageToggleCtrl", gui_test_image_toggle_ctrl,
-    pageList, statusInterface, dialogContext);
-
-  add_test("PaletteCtrl", gui_test_palette_ctrl,
-    pageList, statusInterface, dialogContext);
-
-  add_test("PatternPanel", gui_test_pattern_panel,
-    pageList, statusInterface, dialogContext);
-
-  add_test("ResizeDialog", gui_test_resize_dialog,
-    pageList, statusInterface, dialogContext);
-
-  add_test("SelectedColorCtrl", gui_test_selected_color_ctrl,
-    pageList, statusInterface, dialogContext);
-
-  add_test("Slider", gui_test_slider,
-    pageList, statusInterface, dialogContext);
-
-  add_test("StaticBitmap", gui_test_static_bitmap,
-    pageList, statusInterface, dialogContext);
-
-  add_test("WithLabel", gui_test_with_label,
-    pageList, statusInterface, dialogContext);
+  add_test("AlphaDialog", gui_test_alpha_dialog);
+  add_test("BitmapListCtrl", gui_test_bitmap_list_ctrl);
+  add_test("GradientPanel", gui_test_gradient_panel);
+  add_test("GridDialog", gui_test_grid_dialog);
+  add_test("HSLPanel", gui_test_hsl_panel);
+  add_test("ImageToggleCtrl", gui_test_image_toggle_ctrl);
+  add_test("PaletteCtrl", gui_test_palette_ctrl);
+  add_test("PatternPanel", gui_test_pattern_panel);
+  add_test("ResizeDialog", gui_test_resize_dialog);
+  add_test("SelectedColorCtrl", gui_test_selected_color_ctrl);
+  add_test("Slider", gui_test_slider);
+  add_test("StaticBitmap", gui_test_static_bitmap);
+  add_test("WithLabel", gui_test_with_label);
 }
