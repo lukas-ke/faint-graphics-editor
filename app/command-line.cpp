79bedd21758c9ad9625fa7939feb6b79d4ad1f19
Lukas Kemmer <lukas.kemmer@gmail.com>
2015-06-14 17:08:44 +0200

    Using string constants for command line options.

--- app/command-line.cpp
+++ app/command-line.cpp
@@ -47,52 +47,78 @@ CommandLine::CommandLine() :
   usePenTablet(true)
 {}
 
+const char* CMD_HELP_SHORT = "h";
+const char* CMD_HELP_LONG = "help";
+
+const char* CMD_SILENT_SHORT = "s";
+const char* CMD_SILENT_LONG = "silent";
+
+const char* CMD_NO_PEN_TABLET = "no-tablet";
+
+const char* CMD_NEW_INSTANCE_SHORT = "i";
+const char* CMD_NEW_INSTANCE_LONG = "new-instance";
+
+const char* CMD_NO_SERVER = "no-server";
+
+const char* CMD_SERVER_PORT = "port";
+
+const char* CMD_RUN_SCRIPT = "run";
+
+const char* CMD_SCRIPT_ARG = "arg";
+
 static const wxCmdLineEntryDesc g_cmdLineDesc[] = {
-  {wxCMD_LINE_SWITCH, "h", "help",
+  {wxCMD_LINE_SWITCH, CMD_HELP_SHORT, CMD_HELP_LONG,
     "Displays help on the command line parameters",
    wxCMD_LINE_VAL_NONE, wxCMD_LINE_OPTION_HELP},
 
-  {wxCMD_LINE_SWITCH, "s", "silent",
+  {wxCMD_LINE_SWITCH, CMD_SILENT_SHORT, CMD_SILENT_LONG,
    "Disables the GUI. Requires specifying a script with with --run.",
    wxCMD_LINE_VAL_STRING,
    wxCMD_LINE_PARAM_OPTIONAL},
 
-  {wxCMD_LINE_SWITCH, "", "no-pen-tablet",
+  {wxCMD_LINE_SWITCH, "", CMD_NO_PEN_TABLET,
    "Disable initialization of pen tablet.",
    wxCMD_LINE_VAL_STRING,
    wxCMD_LINE_PARAM_OPTIONAL},
 
-  {wxCMD_LINE_PARAM, "", "", "Image files", wxCMD_LINE_VAL_STRING,
+  {wxCMD_LINE_PARAM, "", "",
+   "Image files",
+   wxCMD_LINE_VAL_STRING,
    wxCMD_LINE_PARAM_OPTIONAL | wxCMD_LINE_PARAM_MULTIPLE},
 
-  {wxCMD_LINE_SWITCH, "i", "new-instance", "Force new instance",
+  {wxCMD_LINE_SWITCH, CMD_NEW_INSTANCE_SHORT, CMD_NEW_INSTANCE_LONG,
+   "Force new instance",
    wxCMD_LINE_VAL_STRING,
    wxCMD_LINE_PARAM_OPTIONAL},
 
-  {wxCMD_LINE_SWITCH, "ii", "no-server",
-   "Prevent this instance from becoming a main app", wxCMD_LINE_VAL_STRING,
+  {wxCMD_LINE_SWITCH, "", CMD_NO_SERVER,
+   "Prevent this instance from becoming a main app",
+   wxCMD_LINE_VAL_STRING,
    wxCMD_LINE_PARAM_OPTIONAL},
 
-  {wxCMD_LINE_OPTION, "", "port",
-   "Specify port used for IPC.", wxCMD_LINE_VAL_STRING,
+  {wxCMD_LINE_OPTION, "", CMD_SERVER_PORT,
+   "Specify port used for IPC.",
+   wxCMD_LINE_VAL_STRING,
    wxCMD_LINE_PARAM_OPTIONAL},
 
-  {wxCMD_LINE_OPTION, "", "run",
+  {wxCMD_LINE_OPTION, "", CMD_RUN_SCRIPT,
    "Run a Python script file after loading images",
-   wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL},
+   wxCMD_LINE_VAL_STRING,
+   wxCMD_LINE_PARAM_OPTIONAL},
 
-  {wxCMD_LINE_OPTION, "arg", "arg",
+  {wxCMD_LINE_OPTION, "", CMD_SCRIPT_ARG,
    "Custom argument stored in ifaint.cmd_arg.", // Fixme: Duplication
    wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL},
 
-  {wxCMD_LINE_NONE, "", "", "",
-   wxCMD_LINE_VAL_NONE, wxCMD_LINE_PARAM_OPTIONAL} // Sentinel
+  { // Sentinel
+    wxCMD_LINE_NONE, "", "", "",
+    wxCMD_LINE_VAL_NONE,
+    wxCMD_LINE_PARAM_OPTIONAL}
 };
 
 void init_command_line_parser(wxCmdLineParser& p){
   p.SetDesc(g_cmdLineDesc);
 }
-
 
 static bool valid_port(const std::string& str){
   if (std::all_of(begin(str), end(str), [](auto v){return std::isdigit(v);})){
@@ -106,14 +132,14 @@ static bool valid_port(const std::string& str){
 
 OrError<CommandLine> get_parsed_command_line(wxCmdLineParser& p){
   CommandLine cmd;
-  cmd.silentMode = p.Found("s");
-  cmd.usePenTablet = !cmd.silentMode && !p.Found("no-pen-tablet");
-  cmd.forceNew = p.Found("i");
-  cmd.preventServer = p.Found("ii");
-  cmd.port = get_string(p, "port", get_default_faint_port());
-  cmd.arg = get_string(p, "arg", "");
+  cmd.silentMode = p.Found(CMD_SILENT_LONG);
+  cmd.usePenTablet = !cmd.silentMode && !p.Found(CMD_NO_PEN_TABLET);
+  cmd.forceNew = p.Found(CMD_NEW_INSTANCE_LONG);
+  cmd.preventServer = p.Found(CMD_NO_SERVER);
+  cmd.port = get_string(p, CMD_SERVER_PORT, get_default_faint_port());
+  cmd.arg = get_string(p, CMD_SCRIPT_ARG, "");
 
-  utf8_string scriptPath = get_string(p, "run");
+  utf8_string scriptPath = get_string(p, CMD_RUN_SCRIPT);
   cmd.scriptPath = make_absolute_file_path(scriptPath);
 
   if (!valid_port(cmd.port.str())){
@@ -122,10 +148,9 @@ OrError<CommandLine> get_parsed_command_line(wxCmdLineParser& p){
   }
 
   if (cmd.silentMode && cmd.scriptPath.NotSet()){
-    return {"Error: --silent requires a script specified with "
-        "--run <scriptname>"};
+    return {space_sep("Error:", no_sep("--", CMD_SILENT_LONG),
+      "requires a script specified with", no_sep("--", CMD_RUN_SCRIPT), "<scriptname>")};
   }
-
 
   for (size_t i = 0; i!= p.GetParamCount(); i++){
     const wxString param(p.GetParam(i));
