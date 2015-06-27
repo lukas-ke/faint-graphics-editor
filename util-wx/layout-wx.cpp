31601379e5b7f1b6dda97d83e09538e9867d31d7
Lukas Kemmer <lukas.kemmer@gmail.com>
2015-06-27 13:08:22 +0200

    Changed default sizer flags to 0 to fix align
    This makes most items in dialogs left aligned again.

--- util-wx/layout-wx.cpp
+++ util-wx/layout-wx.cpp
@@ -36,7 +36,7 @@ SizerItem::SizerItem(wxWindow* item, Proportion p, int flags, int border)
 {}
 
 SizerItem::SizerItem(wxWindow* item)
-  : flags(wxALIGN_CENTER),
+  : flags(0),
     windowItem(item)
 {}
 
@@ -56,7 +56,7 @@ SizerItem::SizerItem(wxSizer* item, Proportion p, int flags, int border)
 {}
 
 SizerItem::SizerItem(wxSizer* item)
-  : flags(wxALIGN_CENTER),
+  : flags(0),
     sizerItem(item)
 {}
 
