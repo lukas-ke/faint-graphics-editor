0fec13dc1aeb6645f13717e2f8f2ff2d0325e898
Lukas Kemmer <lukas.kemmer@gmail.com>
2015-08-20 11:31:46 +0200

    Extended img-render-text.cpp

--- tests/image-tests/img-render-text.cpp
+++ tests/image-tests/img-render-text.cpp
@@ -1,13 +1,14 @@
 // -*- coding: us-ascii-unix -*-
 #include "test-sys/test.hh"
 #include "tests/test-util/file-handling.hh"
-#include "rendering/render-text.hh"
-#include "util/default-settings.hh"
-#include "objects/objtext.hh" // Fixme: Move TextInfoDC elsewhere.
-#include "rendering/text-info-dc.hh"
-#include "geo/tri.hh"
+#include "bitmap/color.hh"
 #include "geo/rect.hh"
 #include "geo/size.hh"
+#include "geo/tri.hh"
+#include "rendering/render-text.hh"
+#include "rendering/text-info-dc.hh"
+#include "util/default-settings.hh"
+#include "text/formatting.hh"
 
 namespace faint{
 
@@ -21,30 +22,87 @@ class StubExpressionContext : public ExpressionContext{
   }
 };
 
+TextBuffer& selected_range(TextBuffer& text, const CaretRange& r){
+  text.select(r);
+  return text;
+}
+
 } // namespace
+
+
 
 void img_render_text(){
   using namespace faint;
-
-  TextBuffer text("Hello world\n");
-
   auto s = default_text_settings();
   TextInfoDC textInfo(s);
-  Bitmap bmp({200, 200});
+  const bool beingEdited = true;
 
   {
-    FaintDC dc(bmp);
+    Bitmap bmp({200, 200}, color_white);
+    {
+      FaintDC dc(bmp);
 
-    StubExpressionContext ctx;
-
-    render_text(dc,
-      text,
-      no_option(),
-      tri_from_rect(Rect(Point(0,0), Size(100, 100))),
-      false,
-      textInfo,
-      ctx,
-      s);
+      StubExpressionContext ctx;
+      TextBuffer text("Hello world\n");
+      render_text(dc,
+        text,
+        no_option(),
+        tri_from_rect(Rect(Point(0,0), Size(200, 100))),
+        beingEdited,
+        textInfo,
+        ctx,
+        s);
+    }
+    save_test_image(bmp, FileName("render-text.png"));
   }
-  save_test_image(bmp, FileName("render-text.png"));
+
+  {
+    Bitmap bmp({200, 200}, color_white);
+    {
+      FaintDC dc(bmp);
+
+      StubExpressionContext ctx;
+      TextBuffer text("Hello selected world\n");
+      text.select(CaretRange(6, 14));
+      render_text(dc,
+        text,
+        no_option(),
+        tri_from_rect(Rect(Point(0,0), Size(200, 100))),
+        beingEdited,
+        textInfo,
+        ctx,
+        s);
+    }
+    save_test_image(bmp, FileName("render-text-selection-basic.png"));
+  }
+
+  {
+    StubExpressionContext ctx;
+    auto render_selected =
+      [&](TextBuffer& text, const CaretRange& r){
+      text.select(r);
+      Bitmap bmp({200, 200}, color_white);
+      {
+        FaintDC dc(bmp);
+        render_text(dc,
+          text,
+          no_option(),
+          tri_from_rect(Rect(Point(0,0), Size(200, 100))),
+          beingEdited,
+          textInfo,
+          ctx,
+          s);
+      }
+      FileName f(no_sep("render-text-selection-",
+          str_uint(r.from), "-to-", str_uint(r.to), ".png"));
+      save_test_image(bmp, f);
+    };
+
+    TextBuffer text("abc\ndef");
+    render_selected(text, CaretRange(0, 3));
+    render_selected(text, CaretRange(0, 4));
+    render_selected(text, CaretRange(0, 5));
+    render_selected(text, CaretRange(0, 6));
+    render_selected(text, CaretRange(0, 7)); // Error: renders identically to 0, 6.
+  }
 }
