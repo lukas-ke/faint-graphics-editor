bc764607896e43071741631b465367c3891cf07a
Lukas Kemmer <lukas.kemmer@gmail.com>
2015-08-23 14:13:36 +0200

    Copy-test for Bitmap.

--- tests/py_ext_tests/test_bitmap.py
+++ tests/py_ext_tests/test_bitmap.py
@@ -26,3 +26,22 @@ class TestBitmap(unittest.TestCase):
         bmp = Bitmap((10, 10))
         bmp.set_pixel((1,1),(255,0,255))
         self.assertEqual(bmp.get_pixel(1,1),(255,0,255,255))
+
+
+    def test_copy(self):
+        from copy import copy
+
+        src = Bitmap((3, 4));
+        pos, color = (2,1), (1,2,3,4)
+        src.set_pixel(pos, color)
+
+        dst = copy(src)
+
+        # Copy is identical - at least one of the pixels :)
+        self.assertEqual(dst.get_pixel(pos), color)
+
+        other_color = 0, 255, 0, 0
+        dst.set_pixel(pos, other_color)
+
+        # Copy is detached from source
+        self.assertEqual(src.get_pixel(pos), color)
