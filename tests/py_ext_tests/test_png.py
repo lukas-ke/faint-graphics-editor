f486078a0b618f5164ec933b791187a925f2435e
Lukas Kemmer <lukas.kemmer@gmail.com>
2015-08-23 16:04:07 +0200

    Added tEXt parsing to png test.

--- tests/py_ext_tests/test_png.py
+++ tests/py_ext_tests/test_png.py
@@ -23,6 +23,23 @@ class TestPng(unittest.TestCase):
         self.assertEqual(tEXt, {})
 
 
+    def test_write_tEXt(self):
+        out_dir = py_ext_tests.make_test_dir(self)
+
+        b1 = faint.Bitmap((5,7))
+        b1.set_pixel((0,0),(255,0,255))
+        written_tEXt = {"hello":"world","good day":"to you"}
+
+        fn = os.path.join(out_dir, "b1.png")
+        faint.write_png(b1, fn, png.RGB, written_tEXt)
+
+        b2, read_tEXt = faint.read_png(fn)
+        self.assertEqual(b2.get_size(), (5,7))
+        self.assertEqual(read_tEXt, written_tEXt)
+
+
     def test_bad_args(self):
+        out_dir = py_ext_tests.make_test_dir(self)
+
         with self.assertRaises(TypeError):
-            faint.write_png("not a bitmap", py_ext_tests.make_test_dir(self), 0)
+            faint.write_png("not a bitmap", out_dir, 0)
