9349c1acf37cd9b452ad63afa7a6fb846fb543bb
Lukas Kemmer <lukas.kemmer@gmail.com>
2015-08-29 12:43:17 +0200

    Sorted py-initialize-ifaint a bit.

--- python/py-initialize-ifaint.cpp
+++ python/py-initialize-ifaint.cpp
@@ -42,20 +42,19 @@
 
 namespace faint{
 
-void add_faint_types(PyObject* module){
+static void add_faint_types(PyObject* module){
   add_type_Bitmap(module);
-  add_type_Tri(module);
+  add_type_FrameProps(module);
+  add_type_ImageProps(module);
+  add_type_Pattern(module);
   add_type_Settings(module);
-  add_type_object(module, SmthType, "Something");
-  add_type_object(module, CanvasType, "Canvas");
-  add_type_object(module, FrameType, "Frame");
+  add_type_Tri(module);
 
   add_gradient_types(module);
-  add_type_ImageProps(module);
-  add_type_FrameProps(module);
-  add_type_Pattern(module);
-
+  add_type_object(module, CanvasType, "Canvas");
+  add_type_object(module, FrameType, "Frame");
   add_type_object(module, GridType, "Grid");
+  add_type_object(module, SmthType, "Something");
 
   PyObject* binds = PyDict_New();
   PyModule_AddObject(module, "_binds", binds);
