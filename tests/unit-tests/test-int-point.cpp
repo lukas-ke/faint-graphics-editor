dcb060a529dcdf06b15ef34fa5cd0fd18a2010a6
Lukas Kemmer <lukas.kemmer@gmail.com>
2015-09-12 09:54:17 +0200

    Replace fixme with c++-conformance comment

--- tests/unit-tests/test-int-point.cpp
+++ tests/unit-tests/test-int-point.cpp
@@ -10,7 +10,7 @@ void test_int_point(){
   static_assert(std::is_literal_type<IntPoint>::value,
     "IntPoint not literal");
 
-  #ifdef _MSC_VER // Fixme: Not supported by clang 3.5.0 with libstdc++
+  #ifdef _MSC_VER // C++14: Not supported by clang 3.5.0 with libstdc++
   static_assert(std::is_trivially_copyable<IntPoint>::value,
     "IntPoint not trivially copyable");
   #endif
