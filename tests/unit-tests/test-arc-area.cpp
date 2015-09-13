3adcfb87739d040a36a303d2dfba69882d9c8fd9
Lukas Kemmer <lukas.kemmer@gmail.com>
2015-09-13 21:37:32 +0200

    Tidied up arc test.

--- tests/unit-tests/test-arc-area.cpp
+++ tests/unit-tests/test-arc-area.cpp
@@ -5,9 +5,10 @@
 
 void test_arc_area(){
   using namespace faint;
-  auto e = 0.0001_eps; // Fixme
+  const auto e = 0.0001_eps;
 
   auto ellipse_area = [](const Radii& r){
+    // For comparisons
     return math::pi * r.x * r.y;
   };
 
@@ -19,44 +20,44 @@ void test_arc_area(){
   NEAR(circle_arc_area(2, 360_deg), math::pi * 2 * 2, e);
   NEAR(arc_area(Radii(1, 1), AngleSpan(0_deg, 90_deg)), math::pi / 4, e);
 
-  {
-    auto a = arc_area(Radii(5, 2), AngleSpan(0_deg, 90_deg));
-    NEAR(a, 7.85398, 0.0001_eps);
-    NEAR(a, ellipse_area(Radii(5, 2)) / 4, 0.0001_eps);
+  { // Wide and low (rx=5, ry=2)
+    NEAR(arc_area(Radii(5, 2), AngleSpan(0_deg, 90_deg)),
+      7.85398, 0.0001_eps);
+    NEAR(arc_area(Radii(5, 2), AngleSpan(0_deg, 90_deg)),
+      ellipse_area(Radii(5, 2)) / 4, 0.0001_eps);
+
+    NEAR(arc_area(Radii(5, 2), AngleSpan(0_deg, 45_deg)),
+      5.95144,
+      0.0001_eps);
+
+    NEAR(arc_area(Radii(5, 2), AngleSpan(0_deg, 180_deg)),
+      ellipse_area(Radii(5,2)) / 2.0, e);
+
+    NEAR(arc_area(Radii(5, 2), AngleSpan(0_deg, 360_deg)),
+      ellipse_area(Radii(5,2)), e);
   }
 
-  NEAR(arc_area(Radii(5, 2), AngleSpan(0_deg, 45_deg)),
-    5.95144,
-    0.0001_eps);
+  { // Tall and thin (opposite radiuses, rx=2, ry=5)
+    NEAR(arc_area(Radii(2, 5), AngleSpan(0_deg, 90_deg)),
+      7.85398, 0.0001_eps);
+    NEAR(arc_area(Radii(2, 5), AngleSpan(0_deg, 90_deg)),
+      ellipse_area(Radii(2, 5)) / 4, 0.0001_eps);
 
-  NEAR(arc_area(Radii(5, 2), AngleSpan(0_deg, 180_deg)),
-    ellipse_area(Radii(5,2)) / 2.0, e);
+    NEAR(arc_area(Radii(2, 5), AngleSpan(0_deg, 45_deg)),
+      1.90253,
+      0.0001_eps);
 
-  NEAR(arc_area(Radii(5, 2), AngleSpan(0_deg, 360_deg)),
-    ellipse_area(Radii(5,2)), e);
+    NEAR(arc_area(Radii(2, 5), AngleSpan(0_deg, 180_deg)),
+      ellipse_area(Radii(2,5)) / 2.0, e);
 
-  // Opposite radiuses
-  {
-    auto a = arc_area(Radii(2, 5), AngleSpan(0_deg, 90_deg));
-    NEAR(a, 7.85398, 0.0001_eps);
-    NEAR(a, ellipse_area(Radii(2, 5)) / 4, 0.0001_eps);
+    NEAR(arc_area(Radii(5, 2), AngleSpan(0_deg, 360_deg)),
+      ellipse_area(Radii(5,2)), e);
   }
 
-  NEAR(arc_area(Radii(2, 5), AngleSpan(0_deg, 45_deg)),
-    1.90253,
-    0.0001_eps);
-
-  NEAR(arc_area(Radii(2, 5), AngleSpan(0_deg, 180_deg)),
-    ellipse_area(Radii(2,5)) / 2.0, e);
-
-  NEAR(arc_area(Radii(5, 2), AngleSpan(0_deg, 360_deg)),
-    ellipse_area(Radii(5,2)), e);
-
-  // Start angles
-  NEAR(arc_area(Radii(5, 2), AngleSpan(10_deg, 32_deg)),
-    2.93098, 0.0001_eps);
-
-  // Start angles
-  NEAR(arc_area(Radii(2, 5), AngleSpan(10_deg, 32_deg)),
-    0.87257, 0.0001_eps);
+  { // With start angles
+    NEAR(arc_area(Radii(5, 2), AngleSpan(10_deg, 32_deg)),
+      2.93098, 0.0001_eps);
+    NEAR(arc_area(Radii(2, 5), AngleSpan(10_deg, 32_deg)),
+      0.87257, 0.0001_eps);
+  }
 }
