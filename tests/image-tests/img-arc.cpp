99305d1decc1d6da0997a342fa78dfe8aab94b8e
Lukas Kemmer <lukas.kemmer@gmail.com>
2015-09-14 20:54:20 +0200

    Added arc image tests.
    It reveals semi-ellipses being deformed compared to the full ellipse.

---
+++ tests/image-tests/img-arc.cpp
@@ -0,0 +1,30 @@
+// -*- coding: us-ascii-unix -*-
+#include "test-sys/test.hh"
+#include "tests/test-util/file-handling.hh"
+#include "bitmap/color.hh"
+#include "geo/arc.hh"
+#include "geo/rect.hh"
+#include "geo/size.hh"
+#include "geo/tri.hh"
+#include "rendering/faint-dc.hh"
+#include "util/default-settings.hh"
+#include "util/setting-id.hh"
+#include "text/formatting.hh"
+
+void img_arc(){
+  using namespace faint;
+  auto arcSettings = default_ellipse_settings();
+  auto ellipseSettings = default_ellipse_settings();
+  ellipseSettings.Set(ts_Fg, Paint(grayscale_rgba(128)));
+
+  for (int i = 0; i < 360; i += 20){
+    const auto tri = tri_from_rect(Rect(Point(5,5), Size(200, 100)));
+    Bitmap bmp({210, 110}, color_white);
+    FaintDC dc(bmp);
+    dc.Ellipse(tri, ellipseSettings);
+    dc.Arc(tri, AngleSpan(0_deg, Angle::Deg(i)), arcSettings);
+    save_test_image(bmp, FileName("render-arc-" +
+      str_int(i, left_pad(4)) +
+      ".png"));
+  }
+}
