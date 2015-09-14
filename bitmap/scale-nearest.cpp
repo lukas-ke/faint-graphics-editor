067c85fb55905aaad4058553591ef037dca55d5e
Lukas Kemmer <lukas.kemmer@gmail.com>
2015-09-14 15:53:19 +0200

    Fixed duplication in scale-nearest.

--- bitmap/scale-nearest.cpp
+++ bitmap/scale-nearest.cpp
@@ -20,31 +20,7 @@ namespace faint{
 
 Bitmap scale_nearest(const Bitmap& src, int scale){
   assert(scale > 0);
-  const int w2 = src.m_w * scale;
-  const int h2 = src.m_h * scale;
-
-  Bitmap scaled(IntSize(w2, h2));
-  int x_ratio = (src.m_w << 16) / scaled.m_w + 1;
-  int y_ratio = (src.m_h << 16) / scaled.m_h + 1;
-  int x2, y2 ;
-  uchar* p_dst = scaled.m_data;
-  const uchar* p_src = src.m_data;
-
-  for (int j = 0; j < h2; j++){
-    for (int i = 0; i < w2; i++){
-      x2 = ((i*x_ratio)>>16);
-      y2 = ((j*y_ratio)>>16);
-
-      uchar* rDst = p_dst + j * (scaled.m_row_stride) + i * ByPP;
-      const uchar* rSrc = p_src + y2 * (src.m_row_stride) + x2 * ByPP;
-
-      *(rDst + 0) = *rSrc;
-      *(rDst + 1) = *(rSrc + 1);
-      *(rDst + 2) = *(rSrc + 2);
-      *(rDst + 3) = *(rSrc + 3);
-    }
-  }
-  return scaled;
+  return scale_nearest(src, src.GetSize() * scale);
 }
 
 Bitmap scale_nearest(const Bitmap& src, const IntSize& newSize){
@@ -55,14 +31,13 @@ Bitmap scale_nearest(const Bitmap& src, const IntSize& newSize){
   Bitmap scaled(IntSize(w2, h2));
   int x_ratio = (src.m_w << 16) / scaled.m_w + 1;
   int y_ratio = (src.m_h << 16) / scaled.m_h + 1;
-  int x2, y2 ;
   uchar* p_dst = scaled.m_data;
   const uchar* p_src = src.m_data;
 
   for (int j = 0; j < h2; j++){
     for (int i = 0; i < w2; i++){
-      x2 = ((i*x_ratio)>>16);
-      y2 = ((j*y_ratio)>>16);
+      int x2 = ((i*x_ratio)>>16);
+      int y2 = ((j*y_ratio)>>16);
 
       uchar* rDst = p_dst + j * (scaled.m_row_stride) + i * ByPP;
       const uchar* rSrc = p_src + y2 * (src.m_row_stride) + x2 * ByPP;
