a7d71268c89b7448d51a5b59aa32a8bef424ec8a
Lukas Kemmer <lukas.kemmer@gmail.com>
2015-11-28 19:02:19 +0100

    Extended unit test.

--- tests/unit-tests/test-int-rect.cpp
+++ tests/unit-tests/test-int-rect.cpp
@@ -35,13 +35,66 @@ void test_int_rect(){
   EQUAL(IntRect(IntPoint(0,0), IntSize(2,2)),
     IntRect(IntPoint(0,0), IntPoint(1,1)));
 
-  // smallest, largest
-  IntRect area1(IntPoint(0,0), IntSize(1,1));
-  IntRect area4(IntPoint(0,0), IntSize(2,2));
-  IntRect area2A(IntPoint(0,0), IntSize(2,1));
-  IntRect area2B(IntPoint(0,0), IntSize(1,2));
-  EQUAL(smallest(area1, area4), area1);
-  EQUAL(largest(area1, area4), area4);
-  EQUAL(largest(area2A, area2B), area2B);
-  EQUAL(smallest(area2A, area2B), area2A);
+  {
+    // Smallest, largest
+    IntRect area1(IntPoint(0,0), IntSize(1,1));
+    IntRect area4(IntPoint(0,0), IntSize(2,2));
+    IntRect area2A(IntPoint(0,0), IntSize(2,1));
+    IntRect area2B(IntPoint(0,0), IntSize(1,2));
+    EQUAL(smallest(area1, area4), area1);
+    EQUAL(largest(area1, area4), area4);
+    EQUAL(largest(area2A, area2B), area2B);
+    EQUAL(smallest(area2A, area2B), area2A);
+  }
+
+  { // Intersection
+    using P = IntPoint;
+    using R = IntRect;
+
+    // Top left
+    EQUAL(intersection(R(P(1,2), P(2,4)),
+        R(P(0, 1), P(1,2))),
+      R(P(1,2), P(1,2)));
+
+    // Top right
+    EQUAL(intersection(R(P(1,2), P(2,4)),
+        R(P(2, 1), P(3,2))),
+      R(P(2,2), P(2,2)));
+
+    // Bottom left
+    EQUAL(intersection(R(P(1,2), P(2,4)),
+        R(P(0, 4), P(1,5))),
+      R(P(1,4), P(1,4)));
+
+    // Bottom right
+    EQUAL(intersection(R(P(1,2), P(2,4)),
+        R(P(2, 4), P(2,5))),
+      R(P(2,4), P(2,4)));
+
+    // Cover
+    EQUAL(intersection(R(P(1,2), P(2,4)),
+        R(P(0,1), P(3,5))),
+      R(P(1,2), P(2,4)));
+
+    // Outside left
+    EQUAL(intersection(R(P(1,2), P(2,4)),
+        R(P(-1,2), P(0,4))),
+      IntRect(P(0,0), IntSize(0,0)));
+
+    // Outside right
+    EQUAL(intersection(R(P(1,2), P(2,4)),
+        R(P(3,2), P(4,4))),
+      IntRect(P(0,0), IntSize(0,0)));
+
+    // Outside above
+    EQUAL(intersection(R(P(1,2), P(2,4)),
+        R(P(1,0), P(2,1))),
+      IntRect(P(0,0), IntSize(0,0)));
+
+    // Outside below
+    EQUAL(intersection(R(P(1,2), P(2,4)),
+        R(P(3,2), P(4,4))),
+      IntRect(P(0,0), IntSize(0,0)));
+
+  }
 }
