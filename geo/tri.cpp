e941fed5570ea6eae67fcbad3cc2a75e716cb642
Lukas Kemmer <lukas.kemmer@gmail.com>
2015-11-29 22:23:46 +0100

    Tidied up Tri.cpp.

--- geo/tri.cpp
+++ geo/tri.cpp
@@ -42,16 +42,16 @@ Tri::Tri(const Point& p0, const Point& p1, coord h)
   : m_p0(p0),
     m_p1(p1)
 {
-  Angle angle = line_angle_cw({p0, p1});
-  coord dy = delta_from_length(h);
+  const auto angle = line_angle_cw({p0, p1});
+  const coord dy = delta_from_length(h);
   m_p2 = m_p0 + Point(dy * cos(angle + pi / 2), dy * sin(angle + pi / 2));
 }
 
 Tri::Tri(const Point& p0, const Angle& angle, const Size& sz)
   : m_p0(p0)
 {
-  coord dx = delta_from_length(sz.w);
-  coord dy = delta_from_length(sz.h);
+  const coord dx = delta_from_length(sz.w);
+  const coord dy = delta_from_length(sz.h);
   m_p1 = m_p0 + Point(dx * cos(angle), dx * sin(angle));
   m_p2 = m_p0 + Point(dy * cos(angle + pi / 2), dy * sin(angle + pi / 2));
 }
@@ -71,10 +71,10 @@ coord Tri::Width() const{
 }
 
 coord Tri::Height() const{
-  Angle angle1 = line_angle_cw({m_p0, m_p1});
-  Angle angle2 = line_angle_cw({m_p0, m_p2});
-  Angle angle3 = angle1 + pi / 2.0;
-  coord h = distance(m_p0, m_p2);
+  const auto angle1 = line_angle_cw({m_p0, m_p1});
+  const auto angle2 = line_angle_cw({m_p0, m_p2});
+  const auto angle3 = angle1 + pi / 2.0;
+  const coord h = distance(m_p0, m_p2);
 
   // Fixme: Seems nutty.
   if (std::fabs(sin(angle2) - sin(angle3)) > 0.1){
@@ -87,7 +87,7 @@ coord Tri::Height() const{
 }
 
 coord Tri::Skew() const{
-  Point p0 = rotate_point(m_p0, -GetAngle(), m_p2);
+  const Point p0 = rotate_point(m_p0, -GetAngle(), m_p2);
   return p0.x - m_p2.x;
 }
 
@@ -122,19 +122,19 @@ bool Tri::Contains(const Point& pt) const{
 }
 
 coord area(const Tri& tri){
-  Tri t2 = rotated(tri, -tri.GetAngle(), tri.P0());
-  coord h = std::fabs(t2.P0().y - t2.P3().y);
+  const auto t2 = rotated(tri, -tri.GetAngle(), tri.P0());
+  const auto h = std::fabs(t2.P0().y - t2.P3().y);
   return tri.Width() * h;
 }
 
 Rect bounding_rect(const Tri& tri){
-  Point p0 = tri.P0();
-  Point p1 = tri.P1();
-  Point p2 = tri.P2();
-  Point p3 = tri.P3();
-  Point minPt = min_coords(p0, p1, p2, p3);
-  Point maxPt = max_coords(p0, p1, p2, p3);
-  return Rect(minPt, maxPt);
+  const auto p0 = tri.P0();
+  const auto p1 = tri.P1();
+  const auto p2 = tri.P2();
+  const auto p3 = tri.P3();
+  const auto minPt = min_coords(p0, p1, p2, p3);
+  const auto maxPt = max_coords(p0, p1, p2, p3);
+  return {minPt, maxPt};
 }
 
 Point center_point(const Tri& t){
@@ -166,7 +166,7 @@ Adj get_adj(const Tri& t1, const Tri& t2){
 }
 
 Radii get_radii(const Tri& tri){
-  return Radii(tri.Width() / 2, tri.Height() / 2);
+  return {tri.Width() / 2, tri.Height() / 2};
 }
 
 Point mid_P0_P1(const Tri& t){
@@ -192,20 +192,18 @@ Point mid_P2_P3(const Tri& t){
 
 static Tri offset_P0_P1(const Tri& t, coord xOff){
   // Fixme: Merge offset_P0_* into offset_aligned
-  Angle a = line_angle_cw(LineSegment(t.P0(), t.P1()));
-
-  coord dx = xOff * cos(a);
-  coord dy = xOff * sin(a);
+  const auto a = line_angle_cw(LineSegment(t.P0(), t.P1()));
+  const coord dx = xOff * cos(a);
+  const coord dy = xOff * sin(a);
 
   return translated(t, dx, dy);
 }
 
 static Tri offset_P0_P2(const Tri& t, coord yOff){
   // Fixme: Merge offset_P0_* into offset_aligned
-  Angle a = line_angle_cw(LineSegment(t.P0(), t.P2()));
-
-  coord dx = yOff * cos(a);
-  coord dy = yOff * sin(a);
+  const auto a = line_angle_cw(LineSegment(t.P0(), t.P2()));
+  const coord dx = yOff * cos(a);
+  const coord dy = yOff * sin(a);
 
   return translated(t, dx, dy);
 }
@@ -218,14 +216,14 @@ Tri rotated(const Tri& t0, const Angle& angle, Point origin){
   Point p0 = rotate_point(t0.P0(), angle, origin);
   Point p1 = rotate_point(t0.P1(), angle, origin);
   Point p2 = rotate_point(t0.P2(), angle, origin);
-  return Tri(p0, p1, p2);
+  return {p0, p1, p2};
 }
 
 Tri scaled(const Tri& t0, const Scale& scale, const Point& origin){
   Point p0 = scale_point(t0.P0(), scale, origin);
   Point p1 = scale_point(t0.P1(), scale, origin);
   Point p2 = scale_point(t0.P2(), scale, origin);
-  return Tri(p0, p1, p2);
+  return {p0, p1, p2};
 }
 
 Tri skewed(const Tri& t, coord skewX){
@@ -240,7 +238,7 @@ Tri skewed(const Tri& t, coord skewX){
   p1.x += skewX;
   p0 = rotate_point(p0, t.GetAngle(), p2);
   p1 = rotate_point(p1, t.GetAngle(), p3);
-  return Tri(p0, p1, p2);
+  return {p0, p1, p2};
 }
 
 Tri translated(const Tri& t, coord tX, coord tY){
@@ -248,11 +246,11 @@ Tri translated(const Tri& t, coord tX, coord tY){
 }
 
 Tri translated(const Tri& t, const Point& off){
-  return Tri(t.P0() + off, t.P1() + off, t.P2() + off);
+  return {t.P0() + off, t.P1() + off, t.P2() + off};
 }
 
 Tri tri_from_rect(const Rect& r){
-  return Tri(r.TopLeft(), r.TopRight(), r.BottomLeft());
+  return {r.TopLeft(), r.TopRight(), r.BottomLeft()};
 }
 
 bool valid(const Tri& t){
@@ -266,11 +264,11 @@ bool rather_zero(const Tri& t){
 }
 
 LineSegment P0_P1(const Tri& t){
-  return LineSegment(t.P0(), t.P1());
+  return {t.P0(), t.P1()};
 }
 
 LineSegment P0_P2(const Tri& t){
-  return LineSegment(t.P0(), t.P2());
+  return {t.P0(), t.P2()};
 }
 
 } // namespace
