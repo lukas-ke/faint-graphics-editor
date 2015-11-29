d1ceb2897dfee76837a0f97db1f6580dc0b07754
Lukas Kemmer <lukas.kemmer@gmail.com>
2015-11-29 20:48:49 +0100

    Simplified Rect.cpp.

--- geo/rect.cpp
+++ geo/rect.cpp
@@ -46,7 +46,7 @@ Rect::Rect(const Point& p, const Size& sz){
 }
 
 Size Rect::GetSize() const{
-  return Size(w, h);
+  return {w, h};
 }
 
 coord Rect::Left() const {
@@ -66,27 +66,28 @@ coord Rect::Bottom() const {
 }
 
 Point Rect::TopLeft() const {
-  return Point(x, y);
+  return {x, y};
 }
 
 Point Rect::TopRight() const {
-  return Point(Right(), y);
+  return {Right(), y};
 }
 
 Point Rect::BottomLeft() const{
-  return Point(x, Bottom());
+  return {x, Bottom()};
 }
 
 Point Rect::BottomRight() const {
-  return Point(Right(), Bottom());
+  return {Right(), Bottom()};
 }
 
 Point Rect::Center() const{
-  return Point(x + (w - 1.0) / 2.0, y + (h - 1.0) / 2.0);
+  return {x + (w - 1.0) / 2.0, y + (h - 1.0) / 2.0};
 }
 
 bool Rect::Contains(const Point& pt) const{
-  return pt.x >= x && pt.y >= y && pt.x <= Right() && pt.y <= Bottom();
+  return pt.x >= x && pt.y >= y &&
+    pt.x <= Right() && pt.y <= Bottom();
 }
 
 bool empty(const Rect& r){
@@ -98,53 +99,23 @@ Rect inflated(const Rect& r, coord d){
 }
 
 Rect inflated(const Rect& r, coord dx, coord dy){
-  return Rect(Point(r.x - dx, r.y - dy), Size(r.w + 2 * dx, r.h + 2 * dy));
+  return {Point(r.x - dx, r.y - dy), Size(r.w + 2 * dx, r.h + 2 * dy)};
 }
 
 Rect intersection(const Rect& r1, const Rect& r2){
-  coord x1 = r1.x < r2.x ? r2.x : r1.x;
-  coord y1 = r1.y < r2.y ? r2.y : r1.y;
-  coord x2 = r1.Right();
-  coord y2 = r1.Bottom();
-
-  if (x2 > r2.Right()){
-    x2 = r2.Right();
-  }
-  if (y2 > r2.Bottom()){
-    y2 = r2.Bottom();
-  }
-
-  coord w2 = x2 - x1 + 1.0;
-  coord h2 = y2 - y1 + 1.0;
-  if (w2 <= 0 || h2 <= 0){
-    return Rect();
-  }
-  return Rect(Point(x1, y1), Size(w2, h2));
+  const auto tl = max_coords(r1.TopLeft(), r2.TopLeft());
+  const auto br = min_coords(r1.BottomRight(), r2.BottomRight());
+  return br.x < tl.x || br.y < tl.y ?
+    Rect() :
+    Rect(tl, br);
 }
 
 bool intersects(const Rect& r1, const Rect& r2){
-  coord x1 = r1.x < r2.x ? r2.x : r1.x;
-  coord y1 = r1.y < r2.y ? r2.y : r1.y;
-  coord x2 = r1.Right();
-  coord y2 = r1.Bottom();
-
-  if (x2 > r2.Right()){
-    x2 = r2.Right();
-  }
-  if (y2 > r2.Bottom()){
-    y2 = r2.Bottom();
-  }
-
-  coord w2 = x2 - x1 + 1.0;
-  coord h2 = y2 - y1 + 1.0;
-  if (w2 < 0 || h2 < 0){
-    return false;
-  }
-  return true;
+  return !empty(intersection(r1, r2));
 }
 
 Rect translated(const Rect& r, const Point& p){
-  return Rect(r.TopLeft() + p, r.GetSize());
+  return {r.TopLeft() + p, r.GetSize()};
 }
 
 } // namespace
